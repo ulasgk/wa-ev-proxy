e103a1158d1bfef3d643bcf9576a79324bdc0dc4
ulasgk <ulasgk@gmail.com>
2025-04-18 08:04:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+import express from "express";
+import fetch from "node-fetch";
 
-const express = require("express");
-const fetch = require("node-fetch");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
